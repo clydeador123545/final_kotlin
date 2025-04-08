19343b67bae5e63bfaf21ac65457a00a2102f536
Ranjet15 <ranjetfortuna21@gmail.com>
2025-04-08 21:05:40 +0800

    update

---
+++ app/src/main/java/com/example/finaloutput/Doctor.kt
@@ -0,0 +1,7 @@
+package com.example.finaloutput
+
+data class Doctor(
+    val name: String,
+    val specialty: String,
+    val email: String
+)
---
+++ app/src/main/java/com/example/finaloutput/doctorlistActivity.java
@@ -0,0 +1,6 @@
+package com.example.finaloutput;
+
+import android.app.Activity;
+
+public class doctorlistActivity extends Activity {
+}
